c8508d893abe438cf4ac05f33bf9529868f0902b
Elliott Morris <131560931+Lampfood@users.noreply.github.com>
2026-01-13 15:36:47 -0600

    Week 1 Assignment 1 correction.py
    Added the comments needed

--- program_1.py
+++ program_1.py
@@ -1,3 +1,5 @@
+#Elliott Morris, 1/13/2026, Hello World
+
 def hello_world():
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
